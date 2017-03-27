859cd6d00f71c905cccb628800e2ef36e76f6513
StevenXue <15994039246@163.com>
2017-03-27 21:50:15 +0800

    Add new java files.

--- xl18/Main.java
+++ xl18/Main.java
@@ -1,36 +1,38 @@
 package xl18;
 
-
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String s= "Dang";
+		String s = "Dang";
 		String str = "";
-		String []temp = new String[2];
+		String[] temp = new String[2];
 		int array[] = new int[2];
-		
+
 		Scanner scan = new Scanner(System.in);
 		String buf = scan.next();
-		temp = buf.split(":");
-		for(int i =0;i<2;i++)
-			array[i] = Integer.parseInt(temp[i]);
+		scan.close();
 		
-		if(array[0]>12 && array[0]<24){
-			int num = array[0]-12;
-			while(num!=0){
-				str = str+s;
+		temp = buf.split(":");
+		for (int i = 0; i < 2; i++)
+			array[i] = Integer.parseInt(temp[i]);
+
+		if (array[0] > 12 && array[0] < 24) {
+			int num = array[0] - 12;
+			while (num != 0) {
+				str = str + s;
 				num--;
 			}
-			if(array[1]==0)
+			if (array[1] == 0)
 				System.out.println(str);
 			else
-				System.out.println(str+s);
-		}else
-		if(array[0]<11 && array[0]>=0 || array[0] ==11 && array[1]<=59 &&array[1]>=0 || array[0] ==12 &&array[1] ==0){
-			System.out.println("Only "+buf+".  Too early to Dang.");
-		}	
- 	}
+				System.out.println(str + s);
+		} else if (array[0] < 11 && array[0] >= 0 || array[0] == 11
+				&& array[1] <= 59 && array[1] >= 0 || array[0] == 12
+				&& array[1] == 0) {
+			System.out.println("Only " + buf + ".  Too early to Dang.");
+		}
+	}
 
 }
--- xl22/Main.java
+++ xl22/Main.java
@@ -2,25 +2,25 @@ package xl22;
 
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
-  public static void main(String[] args) {
-    int array[] = new int[1000];
-    Scanner scan = new Scanner(System.in);
-    int num = scan.nextInt();
-    scan.close();
-    
-    int numOfJishu=0,numOfOushu=0;
-    
-    for(int i =0;i<num;i++){
-       array[i] = scan.nextInt();
-       if(array[i]%2==0){
-         numOfOushu++;
-       }else{
-         numOfJishu++;
-       }
-    }
-    System.out.println(numOfJishu+" "+numOfOushu);  
-   }
+	public static void main(String[] args) {
+		int array[] = new int[1000];
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		scan.close();
+
+		int numOfJishu = 0, numOfOushu = 0;
+
+		for (int i = 0; i < num; i++) {
+			array[i] = scan.nextInt();
+			if (array[i] % 2 == 0) {
+				numOfOushu++;
+			} else {
+				numOfJishu++;
+			}
+		}
+		System.out.println(numOfJishu + " " + numOfOushu);
+	}
 
 }
